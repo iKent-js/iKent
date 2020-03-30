5a29ab693c007320d1e7b471d9c188bbe8d500a5
Serj Golikoff <S75406@yandex.ru>
2020-03-30 23:40:59 +0700

    lesson02

--- js/js.js
+++ js/js.js
@@ -1,22 +1,17 @@
 'use strict';
 
-let money, 
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 50, 
+    income = '"работа с сайтом"',
+    addExpenses = 'такси, кафе, мобильный, прочее',
+    deposit = true,
+    mission = 250000,
+    period = 7,
+    budgetDay = money / 30;
 
 alert('ученик JS 12');
 console.log('Привет ! Я не художник, я только учусь !');
-
-
-
-(function() {
-    var button = document.getElementById('toggle-menu');
-    button.addEventListener('click', function(event) {
-        event.preventDefault();
-        var menu = document.getElementById('main-menu');
-        menu.classList.toggle('is-open');
-    });
-})();
+console.log(typeof money, income, deposit);
+console.log('длина строки addExpenses = ' + addExpenses.length + ' символам');
+console.log('Период равен ' + period + ' месяцам.', 'Цель заработать ' + mission + ' рублей.');
+console.log(addExpenses.toLowerCase().split(' '));
+console.log(budgetDay);
