d33a332750ffbac8cd7af0df6f0b3059de53f34c
Serj Golikoff <S75406@yandex.ru>
2020-03-30 17:47:11 +0700

    Первый коммит

--- js/js.js
+++ js/js.js
@@ -1,2 +1,12 @@
-let num = document.getElementsByClassName('page-header');
+
+(function() {
+    var button = document.getElementById('toggle-menu');
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        var menu = document.getElementById('main-menu');
+        menu.classList.toggle('is-open');
+    });
+})();
+
+var num = document.getElementsByClassName('page-header');
 console.log(num);
