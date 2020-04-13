00413c4c3d7f38f4d03e65734faca793631565e3
Serj Golikoff <S75406@yandex.ru>
2020-04-13 16:23:34 +0700

    not accomplished lesson08

--- js/js.js
+++ js/js.js
@@ -21,6 +21,8 @@ let appData = {
     expenses: {},
     addExpenses: [],
     deposit: false,
+    percentDeposit: 0,
+    moneyDeposit: 0,
     budget: money,
     budgetDay: 0,
     budgetMonth: 0,
@@ -28,6 +30,17 @@ let appData = {
     mission: 250000,
     period: 0,
     asking: function() {
+
+        if(confirm('Есть ли у вас дополнительный источник заработка?')) {
+            let additionalIncome = prompt('Какой у вас дополнительный источник заработка?', 'пишу код');
+
+            let cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?', 20000);
+                while (!isNumber(cashIncome)) {
+                    cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?', 20000);
+                }
+            appData.income[additionalIncome] = +cashIncome;
+        }
+
         let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 
         'clubs, girls, other funs');
         appData.addExpenses = addExpenses.toLowerCase().split(',');
@@ -49,21 +62,6 @@ let appData = {
             sum += +appData.expenses[key];
         }
         return sum;
-
-// donnot touch function below
-        // let expenses = [];
-        // let sum = 0;
-        // let sumStringToNumber;
-        // for (let i = 0; i < 2; i++) {
-        //     expenses[i] = prompt('Введите обязятельную статью расходов ?', 'required payments');
-        //     sumStringToNumber = prompt('Во сколько это обойдется ?', 'sum');
-        //     while (!isNumber(sumStringToNumber)) {
-        //         sumStringToNumber = prompt('Во сколько это обойдется ?', 'sum');
-        //     }
-        //     sum += +sumStringToNumber;
-        // }
-        // console.log(expenses);
-        // return sum;
     },
     getBudget: function (arg1, arg2) {
         return arg1 - arg2;
@@ -84,6 +82,25 @@ let appData = {
         if (appData.budgetDay <= 0) {
             return ('Что то пошло не так ! Че не смеетесь ? Не смешно ? Не поняли, да ? Это РОССИЯ !!!');
         }
+    },
+    getInfoDeposit: function() {
+        if(appData.deposit) {
+            appData.percentDeposit = prompt('Какой годовой процент?', '10');
+            while (!isNumber(appData.percentDeposit)) {
+                appData.percentDeposit = prompt('Какой годовой процент?', '10');
+            }
+        appData.percentDeposit = +appData.percentDeposit;
+
+            appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+            while (!isNumber(appData.moneyDeposit)) {
+                appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+            }
+        appData.moneyDeposit = +appData.moneyDeposit;
+        
+        }
+    },
+    calcSavedMoney: function() {
+        return appData.budgetMonth * appData.period;
     }
 };
 // вызываем метод из объекта который инициализирует работу всего кода )))
@@ -104,11 +121,14 @@ console.log('Расходы за месяц: ' + appData.expensesMonth);
 console.log('Цель будет достигнута за: ' + Math.round(appData.period) + ' месяц(а,ев)');
 console.log(appData.getStatusIncome());
 
-for ( let getObj in appData) {
-    console.log('Наша программа включает в себя данные : ' + 'ключ ' + getObj + ' значение ' + appData[getObj]);
-}
-// от себя добавил )))
-for ( let subObj in appData.expenses) {
-    console.log('Наш внутренний объект включает в себя : ' + 'ключ ' + subObj + 
-    ' значение ' + appData.expenses[subObj]);
-}
+// for ( let getObj in appData) {
+//     console.log('Наша программа включает в себя данные : ' + 'ключ ' + getObj + ' значение ' + appData[getObj]);
+// }
+// // от себя добавил )))
+// for ( let subObj in appData.expenses) {
+//     console.log('Наш внутренний объект включает в себя : ' + 'ключ ' + subObj + 
+//     ' значение ' + appData.expenses[subObj]);
+// }
+appData.getInfoDeposit();
+
+console.log((String(appData.addExpenses)).charAt(0).toUpperCase() + (String(appData.addExpenses[0])).substring(1));
