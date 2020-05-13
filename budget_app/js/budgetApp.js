a5bc51081afd87f370ca7cb7c20266d70829779d
Serj Golikoff <S75406@yandex.ru>
2020-05-13 22:37:16 +0700

    accomplished lesson15

--- budget_app/js/budgetApp.js
+++ budget_app/js/budgetApp.js
@@ -31,25 +31,24 @@ let calculate = document.getElementById('start'),
     getInputData = document.querySelectorAll('.data input[type="text"]'),
     getInputResult = document.querySelectorAll('.result input[type="text"]'),
     periodAmount = document.getElementsByClassName('period-amount')[0];
-
-
-
-    
+   
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
-const AppData = function() {
-    this.income = {};
-    this.incomeMonth = 0;
-    this.addIncome = [];
-    this.expenses = {};
-    this.addExpenses = [];
-    this.deposit = false;
-    this.percentDeposit = 0;
-    this.moneyDeposit = 0;
-    this.budget = 0;
-    this.budgetDay = 0;
-    this.budgetMonth = 0;
-    this.expensesMonth = 0;
-};
+class AppData {
+    constructor() {
+        this.income = {};
+        this.incomeMonth = 0;
+        this.addIncome = [];
+        this.expenses = {};
+        this.addExpenses = [];
+        this.deposit = false;
+        this.percentDeposit = 0;
+        this.moneyDeposit = 0;
+        this.budget = 0;
+        this.budgetDay = 0;
+        this.budgetMonth = 0;
+        this.expensesMonth = 0;
+    }
+}
 AppData.prototype.start = function() {
     this.budget = +salaryAmount.value;
     this.getExpenses();
